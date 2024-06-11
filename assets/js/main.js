a29ba2482f4ce96e5a7bc0bb3d572230a3fc906e
Thac Nguyen Dinh Vu <96606873+nguyendinhvucbg2k3@users.noreply.github.com>
2024-06-11 02:19:23 +0000

    git push origin main

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,58 +1,11 @@
-document.getElementById("form").addEventListener("submit", function (e) {
-  e.preventDefault(); // Prevent the default form submission
-  document.getElementById("message").textContent = "Submitting..";
-  document.getElementById("message").style.display = "block";
-  document.getElementById("submit-button").disabled = true;
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw2ZbXcZvoly-kwjGf9404KfbvJ0BBvcm_Mt8VtcdRhkqnDBf1-lV0d8uCosrZ4k2DThQ/exec'
 
-  // Collect the form data
-  var formData = new FormData(this);
-  var keyValuePairs = [];
-  for (var pair of formData.entries()) {
-    keyValuePairs.push(pair[0] + "=" + pair[1]);
-  }
+const form = document.forms['contact-form']
 
-  var formDataString = keyValuePairs.join("&");
-
-  // Send a POST request to your Google Apps Script
-  fetch(
-    "https://script.google.com/macros/s/AKfycbw2ZbXcZvoly-kwjGf9404KfbvJ0BBvcm_Mt8VtcdRhkqnDBf1-lV0d8uCosrZ4k2DThQ/exec",
-    {
-      redirect: "follow",
-      method: "POST",
-      body: formDataString,
-      headers: {
-        "Content-Type": "text/plain;charset=utf-8",
-      },
-    }
-  )
-    .then(function (response) {
-      // Check if the request was successful
-      if (response) {
-        return response; // Assuming your script returns JSON response
-      } else {
-        throw new Error("Failed to submit the form.");
-      }
-    })
-    .then(function (data) {
-      // Display a success message
-      document.getElementById("message").textContent =
-        "Data submitted successfully!";
-      document.getElementById("message").style.display = "block";
-      document.getElementById("message").style.backgroundColor = "green";
-      document.getElementById("message").style.color = "beige";
-      document.getElementById("submit-button").disabled = false;
-      document.getElementById("form").reset();
-
-      setTimeout(function () {
-        document.getElementById("message").textContent = "";
-        document.getElementById("message").style.display = "none";
-      }, 2600);
-    })
-    .catch(function (error) {
-      // Handle errors, you can display an error message here
-      console.error(error);
-      document.getElementById("message").textContent =
-        "An error occurred while submitting the form.";
-      document.getElementById("message").style.display = "block";
-    });
-});
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thank you! your form is submitted successfully." ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
